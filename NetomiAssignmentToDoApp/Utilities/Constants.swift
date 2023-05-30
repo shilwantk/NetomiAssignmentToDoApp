4c2c360ed6e9967cec37ff9bbb895cf21ed3a72d
Kirti Shilwant <shilwantkirti@gmail.com>
2023-05-30 14:17:45 +0530

    Add alert strings in constants

--- NetomiAssignmentToDoApp/Utilities/Constants.swift
+++ NetomiAssignmentToDoApp/Utilities/Constants.swift
@@ -11,16 +11,29 @@ import UIKit
 struct Constants {
     
     struct VCTitles {
-        static let tasksVC = "Task List"
-        static let addTaskVC = "Add Task"
+        static let TasksVC = "Task List"
+        static let AddTaskVC = "Add Task"
     }
     
     struct TaskStatus {
-        static let pending = "Pending"
+        static let Pending = "Pending"
     }
     
     struct Colors {
-        static let borderGrey = UIColor(red: 0.455, green: 0.467, blue: 0.459, alpha: 0.5).cgColor
-        static let purple = UIColor(red: 0.404, green: 0.314, blue: 0.643, alpha: 1)
+        static let BorderGrey = UIColor(red: 0.455, green: 0.467, blue: 0.459, alpha: 0.5).cgColor
+        static let Purple = UIColor(red: 0.404, green: 0.314, blue: 0.643, alpha: 1)
+        static let LightPurple = UIColor(red: 236/255, green: 230/255, blue: 240/255, alpha: 1)
+    }
+    
+    struct AlertTitleStrings {
+        static let Success = "Success!"
+        static let Failure = "Alert!"
+    }
+    
+    struct AlertMessageString {
+        static let NoNameAdded = "No task title added"
+        static let NoTimeAdded = "No task time selected"
+        static let TaskAddedSuccessfully = "Task successfully added"
+        static let DeleteTaskConfirmation = "Do you want to delete this task? This action can’t be undone."
     }
 }
